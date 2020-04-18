328f717e7bd42fbefc2bbb4937aa991530842608
achristopher4 <59030129+achristopher4@users.noreply.github.com>
2020-04-18 17:01:37 -0400

    Update viewfiles.py

--- Cengage Recursion/viewfiles.py
+++ Cengage Recursion/viewfiles.py
@@ -1,11 +1,14 @@
 import os
 
 def displayFiles(pathname):
+    orginal = pathname
+    pathname = makeReadable(pathname)
+    
     if os.path.isfile(pathname):
-        print('File Name:', pathname)
+        print('File Name:', orginal)
         openFiles(pathname)
     elif os.path.isdir(pathname):
-        print('Directory Name:', pathname)
+        print('Directory Name:', orginal)
         openDirectory(pathname)
 
 def openFiles(pathname):
@@ -21,12 +24,43 @@ def openDirectory(pathname):
         openPath = pathname + str(file)
         if not file.startswith('.DS'):
             if os.path.isfile(openPath):
-                openPath = pathname + str(file)
                 contents = open(openPath, 'r')
                 print('File Name:', str(pathname) + str(file), contents.read())
                 contents.close()
             else:
-                openPath = pathname + str(file) + "/"
-                openDirectory(openPath)
-            
+                openDir = openPath + '/'
+                openDirectory(openDir)
+
+def makeReadable(pathname):
+    seperate = pathname.split('/')
+    removePeriod = []
+    for i in range(len(seperate)):
+        find = seperate[i]
+        if find.startswith('.'):
+            new = find.replace('.','',1)
+            removePeriod.append(new)
+        elif find != '':
+            new = find
+            removePeriod.append(new)
+    lastValue = removePeriod[-1]
+    reconstructionList = []
+    for i in range(len(removePeriod)):
+        determine = removePeriod[i]
+        if determine == lastValue:
+            if os.path.isfile(determine):
+                reconstructionList.append('/'+ determine)
+            elif os.path.isfile(removePeriod[i-1] + '/' + determine):
+                reconstructionList.append('/'+ determine)
+            else:
+                reconstructionList.append('/'+ determine + '/')
+        else:
+            reconstructionList.append('/'+determine)
+    final = ''
+    final = final.join(reconstructionList)
+    return final
+    
+
+        
+
+
            
